5cd9da64f50bf978d74674da09f2ac15fdd0ce2f
Fern <fmenezes098@gmail.com>
2025-08-28 15:18:54 -0300

    Updates

--- src/modules/form/date-change.js
+++
@@ -1,8 +0,0 @@
-import { schedulesDay } from "../schedules/load.js"
-
-//selecionar o input de data
-
-const selectedDate = document.getElementById("date");
-
-//recarrega a lista de horários quando o input de data mudar
-selectedDate.onchange = () => schedulesDay();
--- src/modules/pageload.js
+++
@@ -1,6 +0,0 @@
-import { schedulesDay } from "./schedules/load.js"
-
-//captura o evento enquanto carrega os conteúdos
-document.addEventListener("DOMContentLoaded", function (){
-    schedulesDay()
-})
